7c757d0d970f55006d7f3f6b512741b010cd5a18
PC-coding <partapchauhan8@gmail.com>
2020-11-24 22:25:38 -0500

    progress with firebase

---
+++ src/components/Util/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAsFj1QeZJIsOXVLpSy7wbP29cjc6cK_Vw",
+    authDomain: "bazaar-596f2.firebaseapp.com",
+    databaseURL: "https://bazaar-596f2.firebaseio.com",
+    projectId: "bazaar-596f2",
+    storageBucket: "bazaar-596f2.appspot.com",
+    messagingSenderId: "890418728230",
+    appId: "1:890418728230:web:7d4276bfdc2e5efddb05e7",
+    measurementId: "G-04JE5FMDX0"
+  };
