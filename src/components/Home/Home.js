a302554c56134487177d44e8349f7ac5a295db63
PC-coding <partapchauhan8@gmail.com>
2020-12-01 15:20:29 -0500

    added sliding carousel effect for homepage banners

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import "./Home.css";
 import Product from '../Products/Product';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 
 export default function Home() {
     return(
         <div className="home">
             <div className="home_container">
-            <img
-            className="home_image"
-            src="https://images-na.ssl-images-amazon.com/images/G/01/prime/HOL20_P1_GWH5/HOL_V2/GW/V2/primeholiday_gw_dt_tallhero_3000x1200_holiday_engaged_en_v2._CB403512586_.jpg"
-            alt=""
-            />
+                <Carousel infiniteLoop autoPlay showThumbs={false}>
+                    <img
+                    className="home_image"
+                    src="https://images-na.ssl-images-amazon.com/images/G/01/prime/HOL20_P1_GWH5/HOL_V2/GW/V2/primeholiday_gw_dt_tallhero_3000x1200_holiday_engaged_en_v2._CB403512586_.jpg"
+                    alt=""
+                    />
+                    <img
+                    className="home_image"
+                    src="https://images-na.ssl-images-amazon.com/images/G/01/img20/events/Q4/Holiday/CyberMonday/TrafficDrivers/Q4_2020_CyberMonday_DO_TrafficDrivers_Desktop_Hero_v2-B_3000x1200._CB416740470_.jpg"
+                    alt=""
+                    />
+                    <img
+                    className="home_image"
+                    src="https://images-na.ssl-images-amazon.com/images/G/01/sm/amped/roblox/RobloxDrop4/Desktop_Tablet-Hero_EN_2x._CB416332768_.jpg"
+                    alt=""
+                    />
+                </Carousel>
 
             <div className="home_row">
             <Product
@@ -67,27 +81,27 @@ export default function Home() {
     )
 }
 
-// <div data-test="carousel" length="4" class="carousel carousel-fade z-depth-1" aria-label="carousel">
-//     <div data-test="carousel-inner" class="carousel-inner">
-//         <div data-test="carousel-item" class="carousel-item active carousel-slide-item" style="left: -100%; position: absolute;">
-//             <div class="view" data-test="view">
-//                 <img class="d-block w-100 home__image" src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" alt="First slide"></img>
-//              </div>
-//         </div>
-//         <div data-test="carousel-item" class="carousel-item active carousel-slide-item" style="left: -100%; position: absolute;">
-//             <div class="view" data-test="view">
-//                 <img class="d-block w-100 home__image" src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Computers_1x._CB432469755_.jpg" alt="Second slide"></img>
-//             </div>
-//         </div>
-//         <div data-test="carousel-item" class="carousel-item active carousel-slide-item" style="left: -100%; position: absolute;">
-//             <div class="view" data-test="view">
-//                 <img class="d-block w-100 home__image" src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Sports_en_US_1x._CB431860448_.jpg" alt="Second slide"></img>
-//             </div>
-//         </div>
-//         <div data-test="carousel-item" class="carousel-item active carousel-slide-item" style="left: 0px;">
-//             <div class="view" data-test="view">
-//                 <img class="d-block w-100 home__image" src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_45M_v2_1x._CB432458380_.jpg" alt="Third slide"></img>
-//             </div>
-//         </div>
-//     </div>
-// </div>
+{/* <div data-test="carousel" length="4" class="carousel carousel-fade z-depth-1" aria-label="carousel">
+    <div data-test="carousel-inner" class="carousel-inner">
+        <div data-test="carousel-item" class="carousel-item active carousel-slide-item" style="left: -100%; position: absolute;">
+            <div class="view" data-test="view">
+                <img class="d-block w-100 home__image" src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" alt="First slide"></img>
+             </div>
+        </div>
+        <div data-test="carousel-item" class="carousel-item active carousel-slide-item" style="left: -100%; position: absolute;">
+            <div class="view" data-test="view">
+                <img class="d-block w-100 home__image" src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Computers_1x._CB432469755_.jpg" alt="Second slide"></img>
+            </div>
+        </div>
+        <div data-test="carousel-item" class="carousel-item active carousel-slide-item" style="left: -100%; position: absolute;">
+            <div class="view" data-test="view">
+                <img class="d-block w-100 home__image" src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Sports_en_US_1x._CB431860448_.jpg" alt="Third slide"></img>
+            </div>
+        </div>
+        <div data-test="carousel-item" class="carousel-item active carousel-slide-item" style="left: 0px;">
+            <div class="view" data-test="view">
+                <img class="d-block w-100 home__image" src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_45M_v2_1x._CB432458380_.jpg" alt="Fourth slide"></img>
+            </div>
+        </div>
+    </div>
+</div> */}
